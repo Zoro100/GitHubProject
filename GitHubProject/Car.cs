3b035d96285fda49f824be4526a4b790b70c818a
zuhair khan <zk222ac@hotmail.com>
2017-11-20 10:07:41 +0100

    enter car class

---
+++ GitHubProject/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubProject
+{
+   public class Car
+   {
+       private int _id;
+       private string _name;
+   }
+}
